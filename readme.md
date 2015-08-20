28e3ca05b40c3de30bcb4281710cd0b041121b32
mrazadar <raza.dar@arbisoft.com>
2015-08-20 18:35:51 +0500

    Added google-drive project

---
+++ readme.md
@@ -0,0 +1,156 @@
+Interview mobile Application
+=========
+Getting Started
+---------------
+
+Step-1: (Setup Project)
+=======
+After you have cloned this repo, run the ionic installer to set up your machine
+with the necessary dependencies to run and test this app:
+
+     ```bash
+     $ cd google-drive
+     $ sudo npm install -g cordova ionic gulp
+     $ npm install
+     $ gulp install
+     $ bower install
+     ```
+
+------------------
+Step-2: (Configure Plugins)
+=======
+Then, make sure you have following plugins installed
+
+
+com.ionic.keyboard 1.0.4 "Keyboard"
+cordova-plugin-inappbrowser 1.0.1-dev "InAppBrowser"
+
+------------------
+
+
+Step-3: (Set up google-application on https://console.developers.google.com)
+=======
+
+Create a new project E.g. 'google-drive-test'
+
+open this 'google-drive-test' project. 
+
+Goto APIs & auth -> Credentials
+ 
+ Move to 2nd Tab on top 'OAuth consent screen' 
+ provide a 'Product Name' for your app E.g. "Google Drive Test App 2.0" and hit 'save' button. 
+
+Move back to 1st Tab 'Credentials' on top and click 'Add credentials' button to get a client Id for your application. 
+
+ Select 'OAuth 2.0 client ID' option.
+ Select 'Web-application' option.
+ Put a redirect-uri for your application e.g. 'http://localhost/callback/'
+  
+  Note: Authorized-redirect-uri can be any page where google can redirect user after login. 
+    If you're implementing a purely Javascript based application where you don't have a backend. you can put any valid URL 
+    like 'http:localhost/callback/', this redirect-uri must match with the redirect-uri of your call. 
+     
+     
+ Click 'Create' button and you're finish. :)
+
+
+Step-4: (Authorize with Google and access google-drive-files)
+
+ You just need to call the 'authenticate' method of 'Drive' factory. (This will open goolge-authentication form inside in-app browser plugin)
+ 
+    this.authenticateViaGoogle = function (user) {
+       var defer = $q.defer();
+       var client_id = "553643260895-aoslr91jsj1sc7mpsb4v0ba7eudohi5q.apps.googleusercontent.com";//web-app
+       var scopes = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/userinfo.email'];
+  
+       Drive.authenticate(client_id, scopes, {redirect_uri: 'http://localhost/callback/'})
+           .then(function (response) {
+             if (response) {
+               //console.log("UserInfo: " + JSON.stringify(response));
+               token = response.access_token;
+               gapi.auth.setToken(response);
+               //email= response.authResponse.email;
+               authenticated = true;
+               defer.resolve('authenticated');
+             }
+           },
+           function (error) {
+             console.log("" + error);
+             defer.reject('de-authenticated');
+           });
+       return defer.promise;
+    };
+   
+   
+    Note: how we're passing exactly same 'redirect-uri' in the authorize call. 
+ 
+ once your receive response-token-object. You can use access_token for your authorization scheme. 
+ 
+ Lastly, you just need to set this token in 'gapi' library, to accommodate any further calls using gapi library 
+
+ like this : gapi.auth.setToken(response);
+ 
+ There you go!! 
+ 
+ Now we can simply access our google-drive by using the drive client. 
+ 
+ 
+     Drive.readFiles().then(function (files) {
+       $scope.files = files;
+       console.log("FileRead: success.");
+     }, function () {
+       console.log("FileRead: error.");
+     });
+
+ 'Drive.readFiles' will read all files from google-drive.
+  
+  Similarly 'Drive' factory currently supports these methods.. 
+  
+      ///read all files
+      
+      Drive.readFiles().then(function (files) {
+             $scope.files = files;
+             console.log("FileRead: success.");
+           }, function () {
+             console.log("FileRead: error.");
+           });
+           
+      ///load a single file against Id..
+      
+      Drive.loadFile($scope.file_id).then(function (response) {
+        $scope.file = response;
+        var fpath = $scope.file.metadata.title;
+        $scope.file.metadata.title = fpath.substring(fpath.lastIndexOf('/') + 1, fpath.lastIndexOf('.'));
+        $scope.file_content = $scope.file.content;
+      }, function (error) {
+         console.log(error);
+      });
+      
+      ///save file
+      
+      Drive.saveFile($scope.file.metadata, $scope.file.content).then(function (result) {
+        console.log("FileSaved: successfully.");
+        $state.go('directory');
+      }, function (err) {
+        console.log("FileSaved: error.");
+      });
+      
+      ///delete file
+      
+      Drive.deleteFile(file.id).then(function (resp) {
+        $scope.files.splice($scope.files.indexOf(file), 1);
+        console.log("file deleted successfully.")
+      }, function (error) {
+        console.log(error);
+      });
+      
+      
+      You can still implement whatever you want inside Drive factory.. 
+      
+      It's pretty Simple.. :)
+      
+      
+      
+      
+  
+ 
---
+++ www/js/drive.js
@@ -0,0 +1,330 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+angular.module('starter')
+    .constant('apiKey', null)
+    /*.constant('clientId', '553643260895-aoslr91jsj1sc7mpsb4v0ba7eudohi5q.apps.googleusercontent.com')*/
+    .constant('applicationId', 'AIzaSyB8R8dsQwJSrFTO2kPfKGStEenvPL3dSxo')
+    /*.constant('scope', ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/userinfo.email'])*/
+    .constant('loadApis', {'drive': 'v2'})
+/**
+ * Adapter for exposing gapi as an angular service. This registers a promise that will
+ * resolve to gapi after all the APIs have been loaded.
+ */
+    .factory('googleApi', ['$rootScope', '$window', '$q', 'apiKey', 'loadApis', '$ionicPlatform', function ($rootScope, $window, $q, apiKey, loadApis, $ionicPlatform) {
+
+      var googleApi = $q.defer();
+
+      var init_gapi = function () {
+        $ionicPlatform.ready(function () {
+          ///$rootScope.$apply(function() {
+          var apis = [];
+          if (apiKey) {
+            $window.gapi.client.setApiKey(apiKey);
+          }
+          angular.forEach(loadApis, function (value, key) {
+            apis.push($q.when(gapi.client.load(key, value)));
+          });
+          $q.all(apis).then(function () {
+            googleApi.resolve($window.gapi);
+          });
+        });
+      };
+      init_gapi();
+
+      return googleApi.promise;
+    }])
+    .service('Drive', ['$q', '$cacheFactory', 'googleApi', 'applicationId', '$cordovaOauthUtility',
+      function ($q, $cacheFactory, googleApi, applicationId, $cordovaOauthUtility) {
+
+        // Only fetch fields that we care about
+        var DEFAULT_FIELDS = 'id,title,mimeType,userPermission,editable,copyable,shared,fileSize';
+
+        var cache = $cacheFactory('files');
+
+        /**
+         * Combines metadata & content into a single object & caches the result
+         *
+         * @param {Object} metadata File metadata
+         * @param {String} content File content
+         * @return {Object} combined object
+         */
+        var combineAndStoreResults = function (metadata, content) {
+          var file = {
+            metadata: metadata,
+            content: content
+          };
+          cache.put(metadata.id, file);
+          return file;
+        };
+
+
+        /**
+         * Check if the current token is valid (exists & not expired.)
+         *
+         * @return {Boolean} True if token still valid (not expired)
+         */
+        /*var isTokenValid = function () {
+         var token = gapi.auth.getToken();
+         return (token && Date.now() < token.expires_at);
+         };*/
+
+
+        /**
+         * Attempt authorization.
+         *
+         * @param {Object} request Auth request
+         * @return {Promise} promise that resolves on completion
+         */
+
+        this.authenticate = function (clientId, appScope, options) {
+          /*
+           * Sign into the Google service
+           *
+           * @param    string clientId
+           * @param    array appScope
+           * @param    object options
+           * @return   promise
+           */
+
+            var deferred = $q.defer();
+            if (window.cordova) {
+              var cordovaMetadata = cordova.require("cordova/plugin_list").metadata;
+              if ($cordovaOauthUtility.isInAppBrowserInstalled(cordovaMetadata) === true) {
+                var redirect_uri = "http://localhost/callback";
+                if (options !== undefined) {
+                  if (options.hasOwnProperty("redirect_uri")) {
+                    redirect_uri = options.redirect_uri;
+                  }
+                }
+                var url = 'https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=token';
+                console.log(url);
+                var browserRef = window.open(url, '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+                browserRef.addEventListener("loadstart", function (event) {
+                  if ((event.url).indexOf(redirect_uri) === 0) {
+                    browserRef.removeEventListener("exit", function (event) {
+                    });
+                    browserRef.close();
+                    var callbackResponse = (event.url).split("#")[1];
+                    var responseParameters = (callbackResponse).split("&");
+                    var parameterMap = [];
+                    for (var i = 0; i < responseParameters.length; i++) {
+                      parameterMap[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+                    }
+                    if (parameterMap.access_token !== undefined && parameterMap.access_token !== null) {
+                      deferred.resolve(parameterMap);
+                      //deferred.resolve({ state : parameterMap.state,error : parameterMap.error, access_token: parameterMap.access_token, token_type: parameterMap.token_type, expires_in: parameterMap.expires_in });
+                    } else {
+                      deferred.reject("Problem authenticating");
+                    }
+                  }
+                });
+                browserRef.addEventListener('exit', function (event) {
+                  deferred.reject("The sign in flow was canceled");
+                });
+              } else {
+                deferred.reject("Could not find InAppBrowser plugin");
+              }
+            } else {
+              deferred.reject("Cannot authenticate via a web browser");
+            }
+
+            return deferred.promise;
+        };
+
+
+        this.readFiles = function listFiles() {
+          /*
+           * Print files.
+           **/
+
+          var deffer = $q.defer();
+          var request = gapi.client.drive.files.list({
+            'maxResults': 20
+          });
+
+          request.execute(function (resp) {
+            var files = resp.items;
+            var read_files = [];
+            if (files && files.length > 0) {
+              for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                read_files.push({name: file.title, id: file.id});
+              }
+              deffer.resolve(read_files);
+            } else {
+              deffer.reject("No files found");
+              //appendPre('No files found.');
+              console.log("No files found");
+            }
+          });
+          return deffer.promise;
+        };
+
+        /**
+         * Load a file from Drive. Fetches both the metadata & content in parallel.
+         *
+         * @param {String} fileID ID of the file to load
+         * @return {Promise} promise that resolves to an object containing the file metadata & content
+         */
+        this.loadFile = function (fileId) {
+          var file = cache.get(fileId);
+          if (file) {
+            return $q.when(file);
+          }
+          return googleApi.then(function (gapi) {
+            var metadataRequest = gapi.client.drive.files.get({
+              fileId: fileId,
+              fields: DEFAULT_FIELDS
+            });
+            var contentRequest = gapi.client.drive.files.get({
+              fileId: fileId,
+              alt: 'media'
+            });
+            return $q.all([$q.when(metadataRequest), $q.when(contentRequest)]);
+          }).then(function (responses) {
+            return combineAndStoreResults(responses[0].result, responses[1].body);
+          });
+        };
+
+        /**
+         * Delete a file from Drive. Permanently deletes a file by ID. Skips the trash. The currently authenticated user must own the file.
+         *
+         * @param {String} fileID ID of the file to load
+         * @return {Promise} promise that resolves to an object containing the file metadata & content
+         */
+        this.deleteFile = function (fileId) {
+          var deffer = $q.defer();
+
+          googleApi.then(function (gapi) {
+            var deleteRequest = gapi.client.drive.files.delete({
+              fileId: fileId
+            });
+            deleteRequest.execute(function (success) {
+              deffer.resolve(success);
+            }, function (error) {
+              deffer.reject("File delete error: " + JSON.stringify(error));
+            });
+          });
+          return deffer.promise;
+        };
+        /**
+         * Update a file from Drive. Edit a file by ID. The currently authenticated user must own the file.
+         *
+         * @param {String} fileID ID of the file to edit
+         * @return {Promise} promise that resolves to an object containing the file metadata & content
+         */
+        this.deleteFile = function (fileId) {
+          var deffer = $q.defer();
+
+          googleApi.then(function (gapi) {
+            var deleteRequest = gapi.client.drive.files.delete({
+              fileId: fileId
+            });
+            deleteRequest.execute(function (success) {
+              deffer.resolve(success);
+            }, function (error) {
+              deffer.reject("File delete error: " + JSON.stringify(error));
+            });
+          });
+          return deffer.promise;
+        };
+
+        /**
+         * Save a file to Drive using the mutlipart upload protocol.
+         *
+         * @param {Object} metadata File metadata to save
+         * @param {String} content File content
+         * @return {Promise} promise that resolves to an object containing the current file metadata & content
+         */
+        this.saveFile = function (metadata, content) {
+          return googleApi.then(function (gapi) {
+            var path;
+            var method;
+
+            if (metadata.id) {
+              path = '/upload/drive/v2/files/' + encodeURIComponent(metadata.id);
+              method = 'PUT';
+            } else {
+              path = '/upload/drive/v2/files';
+              method = 'POST';
+            }
+
+            var multipart = new MultiPartBuilder()
+                .append('application/json', JSON.stringify(metadata))
+                .append(metadata.mimeType, content)
+                .finish();
+
+            var uploadRequest = gapi.client.request({
+              path: path,
+              method: method,
+              params: {
+                uploadType: 'multipart',
+                fields: DEFAULT_FIELDS
+              },
+              headers: {'Content-Type': multipart.type},
+              body: multipart.body
+            });
+            return $q.when(uploadRequest);
+          }).then(function (response) {
+            return combineAndStoreResults(response.result, content);
+          });
+        };
+
+        /**
+         * Displays the Drive file picker configured for selecting text files
+         *
+         * @return {Promise} Promise that resolves with the ID of the selected file
+         */
+        this.showPicker = function () {
+          return googleApi.then(function (gapi) {
+            var deferred = $q.defer();
+            var view = new google.picker.View(google.picker.ViewId.DOCS);
+            view.setMimeTypes('text/plain');
+            var picker = new google.picker.PickerBuilder()
+                .setAppId(applicationId)
+                .setOAuthToken(gapi.auth.getToken().access_token)
+                .addView(view)
+                .setCallback(function (data) {
+                  if (data.action == 'picked') {
+                    var id = data.docs[0].id;
+                    deferred.resolve(id);
+                  } else if (data.action == 'cancel') {
+                    deferred.reject();
+                  }
+                })
+                .build();
+            picker.setVisible(true);
+            return deferred.promise;
+          });
+        };
+
+        /**
+         * Displays the Drive sharing dialog
+         *
+         * @param {String} id ID of the file to share
+         */
+        this.showSharing = function (id) {
+          return googleApi.then(function (gapi) {
+            //var deferred = $q.defer();
+            var share = new gapi.drive.share.ShareClient(applicationId);
+            share.setItemIds([id]);
+            share.showSettingsDialog();
+            //return deferred.promise;
+          });
+        };
+
+      }]);
